7f0b973869689fe19a1c06e2697045a01f6ced4f
Franz Herm <93670527+franzherm@users.noreply.github.com>
2024-11-20 18:32:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ BASE_URL = "https://jsonplaceholder.typicode.com/todos/1"
 def fetch_file():
     #headers = {"Authorization": f"Bearer {API_KEY}"}
     #answer = requests.get(f"{BASE_URL}")#, headers=headers)
-    json_str = "{
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-    }"
-    if answer.status_code != 200:
-        raise HTTPException(status_code=answer.status_code, detail="Error fetching file")
+    json_str = '{ \
+    "userId": 1, \
+    "id": 1, \
+    "title": "delectus aut autem", \
+    "completed": false \
+    }'
+    #if answer.status_code != 200:
+    #    raise HTTPException(status_code=answer.status_code, detail="Error fetching file")
 
     return Response(content=json_str, media_type="application/json")
 
