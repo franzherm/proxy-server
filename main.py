14f436aa8ba195d2b91f7fa143f9cd20faa872bf
Franz Herm <93670527+franzherm@users.noreply.github.com>
2024-12-10 10:42:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import FastAPI, HTTPException, Response, File, UploadFile
+from fastapi.responses import FileResponse
 import requests
 import os
 
@@ -10,6 +11,7 @@ TEST_URL = "https://jsonplaceholder.typicode.com/todos/1"
 URL = os.environ.get("ARTIFACT_URL", None)  # follows the scheme: https://<server-url>/artifactory/path/to/file/example_file.csv
 USER = os.environ.get("USER", None)
 API_KEY = os.environ.get("JFROG_API_KEY", None)
+FILE_ROOT_PATH = os.environ.get("FILE_ROOT_PATH", None)
 
 
 @app.get("/")
@@ -22,7 +24,7 @@ def fetch_test_file():
     return Response(content=answer.content, media_type="application/json")
 
 
-@app.get("/file")
+@app.get("/jfrog")
 def fetch_file():
     headers = {"X-JFrog-Art-API": f"{API_KEY}"}
     session = requests.session()
@@ -31,3 +33,20 @@ def fetch_file():
         raise HTTPException(status_code=answer.status_code, detail="Error fetching file")
 
     return Response(content=answer.content, media_type=answer.headers["content-type"])
+
+@app.get("/file")
+async def fetch_local_file(filename: str = "default_file.csv"):
+    return FileResponse(f"{FILE_ROOT_PATH}/{filename}")
+
+@app.put("/upload")
+async def fetch_local_file(file: UploadFile):
+    try:
+        contents = file.file.read()
+        with open(f"{FILE_ROOT_PATH}/{file.filename}", 'wb') as f:
+            f.write(contents)
+    except Exception:
+        raise HTTPException(status_code=500, detail='Something went wrong')
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
